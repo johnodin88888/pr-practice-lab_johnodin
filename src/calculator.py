7ba83f969d4263e7b646b02f125eb03495dc079b
odin.yang <johnodin99johnodin99@gmail.com>
2026-01-12 20:15:28 +0800

    check first pr

--- src/calculator.py
+++ src/calculator.py
@@ -5,3 +5,7 @@ def add(a, b):
 def subtract(a, b):
     """減法"""
     return a - b
+
+def multiply(a, b):
+    """乘法"""
+    return a * b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,11 @@
-from src.calculator import add, subtract
+from src.calculator import add, subtract,multiply
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
